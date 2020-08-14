c29068c41fcc3d63dce5f97aae4ff161fa2a49c3
SaferSephy <tijsvdvelden@hotmail.com>
2020-08-14 16:32:35 +0200

    Create computeCost.m

---
+++ linear regression/computeCost.m
@@ -0,0 +1,11 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+
+m = length(y); % number of training examples
+
+J = 0;
+
+h = X * theta;
+J = 1/(2*length(X))*sum((h-y).^2);
+
+end
