bbd19e05c5c82a239f12d76375dbe7530274c398
SaferSephy <tijsvdvelden@hotmail.com>
2020-08-14 16:32:16 +0200

    Create predictOneVsAll.m

---
+++ logistic regression/one vs all/predictOneVsAll.m
@@ -0,0 +1,20 @@
+function p = predictOneVsAll(all_theta, X)
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1).
+
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+tmp = sigmoid(X*all_theta');
+
+%use max to get the index of the best prediction which equals the class
+[value,index] = max(tmp, [], 2);
+p = index;
+
+end
